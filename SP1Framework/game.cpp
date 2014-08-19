69bf2b115b42b915f5336eee5ebb9efd7bbf9312
Rui Peng <ongruipeng@gmail.com>
2014-08-19 13:03:22 +0800

    players can now lose the game

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -12,7 +12,12 @@ double timeFall;
 bool keyPressed[K_COUNT];
 COORD charLocation;
 COORD consoleSize;
-COORD sackLocation;
+COORD sack1Location;
+COORD sack2Location;
+COORD sack3Location;
+COORD sack4Location;
+COORD sack5Location;
+int sackDecider;
 void init()
 {
     // Set precision for floating point output
@@ -34,9 +39,11 @@ void init()
     // set the character to be in the center of the screen.
     charLocation.X = consoleSize.X / 2;
     charLocation.Y = 52;
-	sackLocation.X = 20;
-	sackLocation.Y = 0;
-    elapsedTime = 0.0;
+	sack1Location.X = 0;
+	sack2Location.X = 0;
+	sack3Location.X = 0;
+	sack4Location.X = 0;
+	elapsedTime = 0.0;
 }
 
 void shutdown()
@@ -61,24 +68,70 @@ void update(double dt)
     deltaTime = dt;
 
 	timeFall = elapsedTime + 1.0;
-	if ( elapsedTime < timeFall )
+	if ( elapsedTime < timeFall )    //magic numbers are 3, 59, 21, 40
 	{
-		
-		if ( sackLocation.Y < 48 )
+		sackDecider = rand() % 4 + 1; // decide where sacks will spawn
+		switch ( sackDecider )
 		{
-			sackLocation.Y++;
-
+			case 1: sack1Location.X = 2;
+				break;
+			case 2: sack2Location.X = 21;
+				break;
+			case 3: sack3Location.X = 40;
+				break;
+			case 4: sack4Location.X = 59;
+				break;
 		}
-		if ( sackLocation.Y == 48 )
+		
+		// sack drop
+		if ( sack1Location.Y < 52 && sack1Location.X != 0 && sack1Location.X != 1)  
 		{
-			sackLocation.X = 0;
-			sackLocation.Y = 0;
+			sack1Location.Y++, sack1Location.Y++, sack1Location.Y++, sack1Location.Y++;
+		}
+		if ( sack2Location.Y < 52  && sack2Location.X != 0 && sack2Location.X != 1)
+		{
+			sack2Location.Y++;
+		}
+		if ( sack3Location.Y < 52 && sack3Location.X != 0 && sack3Location.X != 1)
+		{
+			sack3Location.Y++;
+		}
+		if ( sack4Location.Y < 52 && sack4Location.X != 0 && sack4Location.X != 1)
+		{
+			sack4Location.Y++;
+		}
+		
+		if ( sack1Location.Y == 48 && charLocation.X == 2 )           // sack disappear after collected X=0 reset, X=1 game over
+		{
+			sack1Location.X = 0;
+			sack1Location.Y = 0;
+		}
+		if ( sack2Location.Y == 48 && charLocation.X == 21)
+		{
+			sack2Location.X = 0;
+			sack2Location.Y = 0;
+		}
+		if ( sack3Location.Y == 48 && charLocation.X == 40 )
+		{
+			sack3Location.X = 0;
+			sack3Location.Y = 0;
+		}
+		if ( sack4Location.Y == 48 && charLocation.X == 59 )
+		{
+			sack4Location.X = 0;
+			sack4Location.Y = 0;
+		}
+
+		//game over
+		if ( sack1Location.Y == 52 || sack2Location.Y == 52 || sack3Location.Y == 52 || sack4Location.Y == 52 )
+		{
+			sack1Location.X = 1, sack2Location.X = 1, sack3Location.X = 1, sack4Location.X = 1;
 		}
 	}
 
 	// Updating the location of the character based on the key press
 	
-	if (keyPressed[K_LEFT] && charLocation.X > 3)
+	if (keyPressed[K_LEFT] && charLocation.X > 2)
     {
         Beep(1440, 30);
 		for ( int a = 0; a < 19; a++ )
@@ -115,14 +168,27 @@ void render()
 	                        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
 	                        };
 	
-	if ( sackLocation.X != 0 )
+	if ( sack1Location.X != 0 && sack1Location.X != 1)
 	{
-	gotoXY(sackLocation); // sack going down
-	std::cout << "sack";
-	gotoXY(sackLocation); // sack going down
-	std::cout << "sack";
+		gotoXY(sack1Location); // sack going down
+		std::cout << "sack";
 	}
-		
+	if ( sack2Location.X != 0 && sack2Location.X != 1 )
+	{
+		gotoXY(sack2Location);
+		std::cout << "sack";
+	}
+	if ( sack3Location.X != 0 && sack3Location.X != 1)
+	{
+		gotoXY(sack3Location);
+		std::cout << "sack";
+	}
+	if ( sack4Location.X != 0 && sack4Location.X != 1)
+	{
+		gotoXY(sack4Location);
+		std::cout << "sack";
+	}
+	
     // render time taken to calculate this frame
     gotoXY(70,0);
     colour(0x1A);
@@ -134,8 +200,11 @@ void render()
 
     // render character
     gotoXY(charLocation);
-    colour(0x0C);
     std::cout <<"___________________";
 
-    
+    if ( sack1Location.X == 1 || sack2Location.X == 1 || sack3Location.X == 1 || sack4Location.X == 1)
+	{
+		gotoXY(charLocation);
+		std::cout << "gameover";
+	}
 }
