ee8e1ba3f772196988755a3bda4f6117554d3d78
Rui Peng <ongruipeng@gmail.com>
2014-08-28 15:25:20 +0800

    updated instructions

--- SP1Framework/instruction.cpp
+++ SP1Framework/instruction.cpp
@@ -6,13 +6,27 @@ int instruction()
 	int input = 0;
 	COORD c;
 	c.X = 0;
-	c.Y = 0;
-	writeToBuffer(c,"INSTRUCTIONS");
-    c.Y++;
-	writeToBuffer(c,"Use left and right arrow keys to move character to catch the rice sacks.");
-    c.Y++;
-	writeToBuffer(c,"Catch as many rice sacks as you can, if the rice sack hits the floor it is game over!");
+	c.Y = 1;
+	writeToBuffer(c,"  ___ _  _ ___ _____ ___ _   _  ___ _____ ___ ___  _  _ ___ ");
 	c.Y++;
+	writeToBuffer(c," |_ _| \\| / __|_   _| _ \\ | | |/ __|_   _|_ _/ _ \\| \\| / __|");
+	c.Y++;
+	writeToBuffer(c,"  | || .` \\__ \\ | | |   / |_| | (__  | |  | | (_) | .` \\__ \\ ");
+	c.Y++;
+	writeToBuffer(c," |___|_|\\_|___/ |_| |_|_\\\\___/ \\___| |_| |___\\___/|_|\\_|___/");
+
+
+    c.Y++;
+	writeToBuffer(c,"Use LEFT and RIGHT arrow keys to move character to catch the rice sacks.");
+    c.Y++;
+	writeToBuffer(c,"Catch as many rice sacks as you can, if the rice sack hits the floor you lose a life");
+	c.Y++;
+	writeToBuffer(c, "Avoid catching vases as it costs you a life ");
+	c.Y++;
+	writeToBuffer(c, "Try to catch bonus sacks that gives more points");
+	c.Y++;
+	writeToBuffer(c, "Press space while in game to pause" );
+	c.Y += 3;
 	writeToBuffer(c,"Press 5 to return to main menu");
 	flushBufferToConsole();
 	while ( input != BACKTOMAINMENU )//ask the player to press 5 to return to main menu
